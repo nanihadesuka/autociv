12bea095c3b25e56aa1bf399a8a5a0e5f0ca5bb0
nani <nani@>
2019-10-21 02:42:11 +0200

    autocivCV.js rewrite & version bump

--- gui/common/!!!autocivCL.js
+++ gui/common/!!!autocivCL.js
@@ -19,37 +19,26 @@ function patchApplyN(oldFnName, patchFn)
 }
 
 let autocivCL = {
-    "engine_version": Engine.GetEngineInfo().engine_version,
     "Engine": {
         "PushGuiPage": (xmlFile, data, callbackfunction) =>
         {
-            switch (autocivCL.engine_version)
-            {
-                case "0.0.23":
-                    let temp = callbackfunction ?
-                        Object.assign({}, data, { "callback": callbackfunction.name }) :
-                        data;
-                    Engine.PushGuiPage(xmlFile, temp);
-                    break;
-                default:
-                    Engine.PushGuiPage(xmlFile, data, callbackfunction);
-                    break;
-            }
+            if (!autociv_is24)
+                return Engine.PushGuiPage(xmlFile, data, callbackfunction);
+
+            let temp = callbackfunction ?
+                Object.assign({}, data, { "callback": callbackfunction.name }) :
+                data;
+            Engine.PushGuiPage(xmlFile, temp);
         },
         "PopGUIPage": (data, hasCallback = false) =>
         {
-            switch (autocivCL.engine_version)
-            {
-                case "0.0.23":
-                    if (hasCallback)
-                        Engine.PopGuiPageCB(data);
-                    else
-                        Engine.PopGuiPage(data);
-                    break;
-                default:
-                    Engine.PopGuiPage(data);
-                    break;
-            }
+            if (autociv_is24)
+                return Engine.PopGuiPage(data);
+
+            if (hasCallback)
+                Engine.PopGuiPageCB(data);
+            else
+                Engine.PopGuiPage(data);
         }
     }
 };
