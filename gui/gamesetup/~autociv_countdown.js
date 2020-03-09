21a3db2a0ad85ccf7cf547eae9d5b3a6393d56fd
nani <nani@>
2020-03-09 23:49:39 +0100

    improved countdown and handled mor edges cases

--- gui/gamesetup/~autociv_countdown.js
+++ gui/gamesetup/~autociv_countdown.js
@@ -1,81 +1,88 @@
 var g_autociv_countdown = {
-    "active": false,
-    "default_time": parseInt(Engine.ConfigDB_GetValue("user", "autociv.gamesetup.countdown.time"), 10),
-    "set_time": undefined,
-    "time": undefined,
-    "timeoutid": null,
-    "next": function ()
-    {
-        if (this.time < 1)
-        {
-            this.stopCountdown();
-            launchGame();
-            return;
-        }
+	"active": false,
+	"default_time": parseInt(Engine.ConfigDB_GetValue("user", "autociv.gamesetup.countdown.time"), 10),
+	"set_time": undefined,
+	"time": undefined,
+	"timeoutid": null,
+	"next": function ()
+	{
+		if (this.time < 0.01)
+		{
+			this.stopCountdown();
+			// launchGame();
+			return;
+		}
 
-        this.timeoutid = setTimeout(() =>
-        {
-            sendMessage(`Game will start in ${this.time--} seconds.`);
-            this.next()
-        }, 1000);
-    },
-    "startCountdown": function (time = this.default_time)
-    {
-        this.stopCountdown();
-        this.set_time = time;
-        this.time = time;
-        if (this.valid())
-            this.next();
-    },
-    "resetCountdown": function ()
-    {
-        this.startCountdown(this.set_time)
-    },
-    "stopCountdown": function ()
-    {
-        clearTimeout(this.timeoutid);
-    },
-    "valid": function ()
-    {
-        return game.is.full() && game.is.allReady() &&
-            (game.get.numberOfSlots() == 2 ? !game.is.rated() : true);
-    },
-    "gameUpdate": function ()
-    {
-        if (!this.active)
-            return;
+		this.timeoutid = setTimeout(() =>
+		{
+			sendMessage(`Game will start in ${this.time--} seconds.`);
+			this.next()
+		}, 1000);
+	},
+	"startCountdown": function (time)
+	{
+		this.stopCountdown();
+		this.set_time = time;
+		this.time = time;
+		if (this.valid())
+			this.next();
+	},
+	"resetCountdown": function ()
+	{
+		this.startCountdown(this.set_time)
+	},
+	"stopCountdown": function ()
+	{
+		clearTimeout(this.timeoutid);
+	},
+	"valid": function ()
+	{
+		return game.is.full() && game.is.allReady() &&
+			(game.get.numberOfSlots() == 2 ? !game.is.rated() : true);
+	},
+	"gameUpdate": function ()
+	{
+		if (!this.active)
+			return;
 
-        if (this.valid())
-            this.resetCountdown();
-        else
-            this.stopCountdown();
-    },
-    "toggle": function (active = !this.active, time = this.default_time)
-    {
-        this.active = active;
-        this.set_time = time;
-        if (active)
-        {
-            selfMessage(`Countdown enabled: ${time} seconds.`);
-            this.startCountdown(this.set_time);
-        }
-        else
-        {
-            selfMessage(`Countdown disabled.`);
-            this.stopCountdown();
-        }
-    },
-    "init": function ()
-    {
-        if (g_IsController && Engine.ConfigDB_GetValue("user", "autociv.gamesetup.countdown.enabled") == "true")
-            g_autociv_countdown.toggle(true);
-    }
-}
+		if (this.valid())
+			this.resetCountdown();
+		else
+			this.stopCountdown();
+	},
+	"toggle": function (active = !this.active, time = this.default_time)
+	{
+		this.active = active;
+		if (active)
+		{
+			selfMessage(`Countdown set to ${time} seconds.`);
+			this.startCountdown(time);
+		}
+		else
+		{
+			selfMessage(`Countdown disabled.`);
+			this.stopCountdown();
+		}
+	},
+	"init": function ()
+	{
+		if (g_IsController && Engine.ConfigDB_GetValue("user", "autociv.gamesetup.countdown.enabled") == "true")
+			g_autociv_countdown.toggle(true);
+	}
+};
 
 autociv_patchApplyN("updateGameAttributes", function (target, that, args)
 {
-    let result = target.apply(that, args);
-    if (g_IsController)
-        g_autociv_countdown.gameUpdate();
-    return result;
+	let result = target.apply(that, args);
+	if (g_IsController)
+		g_autociv_countdown.gameUpdate();
+	return result;
+});
+
+autociv_patchApplyN("handleReadyMessage", function (target, that, args)
+{
+	let result = target.apply(that, args);
+	if (g_IsController)
+		g_autociv_countdown.gameUpdate();
+	return result;
 });
