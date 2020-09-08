90ad0528a2002b5e8ceba32e1bad0d408f088001
nani <nani@>
2020-09-08 14:57:24 +0200

    escape markup text after transformation not before + some name refactoing

--- gui/common/~autociv_SimpleMarkup.js
+++ gui/common/~autociv_SimpleMarkup.js
@@ -6,27 +6,34 @@ function autociv_SimpleMarkup(inputText)
 {
     return inputText.split("\n").
         filter(l => l.trim().length !== 0).
-        map(l => l.replace(/\t/g, "    ").replace(/\\/g, "\\\\").replace(/\[/g, "\\[")).
+        map(l => l.replace(/\t/g, "    ")).
         map((line, index) => autociv_SimpleMarkup.makeLine(line, 0, index)).
         join("\n")
 }
 
-// Has no concept of other possible lines (no internal state)
-autociv_SimpleMarkup.makeLine = function (inputLine, depth, index)
+autociv_SimpleMarkup.escape = function (text)
 {
-    // return inputLine
-    let d = (text) => autociv_SimpleMarkup.makeLine(text, depth + 1, index)
+    return text.replace(/\\/g, "\\\\").replace(/\[/g, "\\[")
+}
+
+/**
+ * Function has no concept of other possible lines (no internal state)
+ * Must return valid pyrogenesis text string format
+ */
+autociv_SimpleMarkup.makeLine = function (input, depth, lineIndex)
+{
+    let d = (text) => autociv_SimpleMarkup.makeLine(text, depth + 1, lineIndex)
     let grabber = (regex, text) => regex.exec(text).slice(1)
-    let [prefix, mainText] = grabber(/^( *)(.*)$/, inputLine)
+    let [prefix, body] = grabber(/^( *)(.*)$/, input)
 
     // Title
-    if (/^#+ .*$/.test(mainText) && depth < 2)
+    if (/^#+ .*$/.test(body) && depth < 2)
     {
-        let [hash, text] = grabber(/^(#+) (.*)$/, mainText)
+        let [hash, text] = grabber(/^(#+) (.*)$/, body)
 
         let out = size => depth != 0 ?
             `${prefix}[font="sans-bold-${size}"]${d(text)}[/font]` :
-            index == 0 ?
+            lineIndex == 0 ?
                 `${prefix}[font="sans-bold-${size}"]${d(text)}[/font]\n` :
                 `\n\n${prefix}[font="sans-bold-${size}"]${d(text)}[/font]\n`;
 
@@ -43,13 +50,13 @@ autociv_SimpleMarkup.makeLine = function (inputLine, depth, index)
         }
     }
     // Bullet point
-    if (/^- .*$/.test(mainText) && depth < 2)
+    if (/^- .*$/.test(body) && depth < 2)
     {
-        let [slash, text] = grabber(/^(-) *(.*)$/, mainText)
+        let [slash, text] = grabber(/^(-) *(.*)$/, body)
         return `${prefix}[font="sans-bold-16"]•[/font] ${d(text)}`
     }
     else
     {
-        return `${prefix}${mainText}`
+        return autociv_SimpleMarkup.escape(`${prefix}${body}`)
     }
 }
