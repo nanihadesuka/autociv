76ca488c78f4682a21d9b38e72b87dc0afae2707
nani <nani@>
2021-01-22 07:32:32 +0100

    remove svn a24 as compatible for now\n fix countdown not detecting some changes

--- gui/gamesetup/~autociv_countdown.js
+++ gui/gamesetup/~autociv_countdown.js
@@ -8,24 +8,24 @@ var g_autociv_countdown = {
 	{
 		if (this.time < 0)
 		{
-			this.stopCountdown();
-			launchGame();
-			return;
+			this.stopCountdown()
+			// launchGame()
+			return
 		}
 
 		this.timeoutid = setTimeout(() =>
 		{
-			sendMessage(`Game will start in ${this.time--} seconds.`);
+			sendMessage(`Game will start in ${this.time--} seconds.`)
 			this.next()
-		}, 1000);
+		}, 1000)
 	},
 	"startCountdown": function (time)
 	{
-		this.stopCountdown();
-		this.set_time = time;
-		this.time = time;
+		this.stopCountdown()
+		this.set_time = time
+		this.time = time
 		if (this.valid())
-			this.next();
+			this.next()
 	},
 	"resetCountdown": function ()
 	{
@@ -33,56 +33,64 @@ var g_autociv_countdown = {
 	},
 	"stopCountdown": function ()
 	{
-		clearTimeout(this.timeoutid);
+		clearTimeout(this.timeoutid)
 	},
 	"valid": function ()
 	{
 		return game.is.full() && game.is.allReady() &&
-			(game.get.numberOfSlots() == 2 ? !game.is.rated() : true);
+			(game.get.numberOfSlots() == 2 ? !game.is.rated() : true)
 	},
 	"gameUpdate": function ()
 	{
 		if (!this.active)
-			return;
+			return
 
 		if (this.valid())
-			this.resetCountdown();
+			this.resetCountdown()
 		else
-			this.stopCountdown();
+			this.stopCountdown()
 	},
 	"toggle": function (active = !this.active, time = this.default_time)
 	{
-		this.active = active;
+		this.active = active
 		if (active)
 		{
-			selfMessage(`Countdown set to ${time} seconds.`);
-			this.startCountdown(time);
+			selfMessage(`Countdown set to ${time} seconds.`)
+			this.startCountdown(time)
 		}
 		else
 		{
-			selfMessage(`Countdown disabled.`);
-			this.stopCountdown();
+			selfMessage(`Countdown disabled.`)
+			this.stopCountdown()
 		}
 	},
 	"init": function ()
 	{
 		if (g_IsController && Engine.ConfigDB_GetValue("user", "autociv.gamesetup.countdown.enabled") == "true")
-			g_autociv_countdown.toggle(true);
+			g_autociv_countdown.toggle(true)
 	}
-};
+}
 
-autociv_patchApplyN("updateGameAttributes", function (target, that, args)
+autociv_patchApplyN("handlePlayerAssignmentMessage", function (target, that, args)
 {
-	let result = target.apply(that, args);
+	let result = target.apply(that, args)
 	if (g_IsController)
-		g_autociv_countdown.gameUpdate();
-	return result;
-});
+		g_autociv_countdown.gameUpdate()
+	return result
+})
+
+autociv_patchApplyN("handleGamesetupMessage", function (target, that, args)
+{
+	let result = target.apply(that, args)
+	if (g_IsController)
+		g_autociv_countdown.gameUpdate()
+	return result
+})
 
 autociv_patchApplyN("handleReadyMessage", function (target, that, args)
 {
-	let result = target.apply(that, args);
+	let result = target.apply(that, args)
 	if (g_IsController)
-		g_autociv_countdown.gameUpdate();
-	return result;
-});
+		g_autociv_countdown.gameUpdate()
+	return result
+})
