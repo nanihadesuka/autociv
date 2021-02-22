5612009fc15237b955ea576e89c54b4b81bc7d44
nani <nani@>
2021-02-23 00:45:50 +0100

    Minimap expand compatible with a24

--- gui/session/autociv_minimapExpand.js
+++ gui/session/autociv_minimapExpand.js
@@ -2,22 +2,35 @@ var autociv_minimapExpand = {
 	"toggle": function ()
 	{
 		if (!this.defaultSize)
+		{
 			this.defaultSize = this.GUI.size;
+			this.defaultSizeParent = this.GUIParent.size;
+		}
 
-		this.GUI.size = this.expanded ? this.defaultSize : this.getExpandedSize();
+		if (this.expanded)
+		{
+			this.GUIParent.size = this.defaultSizeParent;
+			this.GUI.size = this.defaultSize;
+		}
+		else
+		{
+			let windowSize = Engine.GetGUIObjectByName("session").getComputedSize();
+			let halfHeight = (windowSize.bottom - windowSize.top) / 2 - 70;
+
+			let sizeParent = `50% 50% 50% 50%`;
+			this.GUIParent.size = sizeParent;
+
+			let size = `50%-${halfHeight - 75} 50%-${halfHeight} 50%+${halfHeight - 75} 50%+${halfHeight - 150}`;
+			this.GUI.size = size;
+		}
+
 		this.expanded = !this.expanded;
 	},
-	"getExpandedSize": function ()
-	{
-		let windowSize = Engine.GetGUIObjectByName("session").getComputedSize();
-		let halfHeight = (windowSize.bottom - windowSize.top) / 2 - 70;
-		return `50%-${halfHeight - 75} 50%-${halfHeight} 50%+${halfHeight - 75} 50%+${halfHeight - 150}`;
-	},
 	"expanded": false,
-	"defaultSize": undefined,
 	get GUI()
 	{
 		return "_GUIObject" in this ? this._GUIObject :
 			this._GUIObject = Engine.GetGUIObjectByName("minimapPanel");
-	}
+	},
+	get GUIParent() { return this.GUI.parent }
 };
