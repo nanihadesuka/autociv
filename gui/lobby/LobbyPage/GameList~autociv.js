deccd8274c45adae2f6ee58662fa42392d4dc17f
nani <nani@>
2021-02-23 21:58:14 +0100

    showHostName option for lobby

--- gui/lobby/LobbyPage/GameList~autociv.js
+++ gui/lobby/LobbyPage/GameList~autociv.js
@@ -1,20 +1,17 @@
 
-// WE NO LONGER KNOWN IF GAME IF STUN OR PORTFORWARD SO THIS IS USELESS
-// autociv_patchApplyN(GameList.prototype, "rebuildGameList", function (target, that, args)
-// {
-//     let result = target.apply(that, args);
+// NOTE: in a24 we can no longer know if host is stun or port forward
+autociv_patchApplyN(GameList.prototype, "rebuildGameList", function (target, that, args)
+{
+    let result = target.apply(that, args);
 
-//     if (Engine.ConfigDB_GetValue("user", "autociv.lobby.gamelist.markStunGames") == "true")
-//     {
-//         let gamesBox = Engine.GetGUIObjectByName(g_autociv_isFGod ? "gameList" : "gamesBox");
-//         gamesBox.list_gameName = gamesBox.list_gameName.map((v, i) =>
-//         {
-//             if (i > that.gameList.length - 1)
-//                 return v;
+    if (Engine.ConfigDB_GetValue("user", "autociv.lobby.gamelist.showHostName") != "true")
+        return;
 
-//             return `${v} [color="90 90 90"]\\ ${that.gameList[i].stunIP ? "stun" : "port forward"}[/color]`;
-//         });
-//         gamesBox.list = gamesBox.list;
-//     }
-//     return result;
-// })
+    let gamesBox = Engine.GetGUIObjectByName("gamesBox");
+    gamesBox.list_gameName = gamesBox.list_gameName.map((v, i) =>
+        `${v} [color="90 90 90"]\\ - ${that.gameList[i]?.stanza?.hostUsername ?? ""}[/color]`
+    );
+
+    gamesBox.list = gamesBox.list;
+    return result;
+})
