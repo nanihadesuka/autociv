bdc77497dadc2462fb2508677fe2f8105184aad9
nani <nani@>
2021-03-12 22:58:45 +0100

    Spread over tick time the first time load for lobby with many pending messages.

--- gui/lobby/XmppMessages~autociv.js
+++ gui/lobby/XmppMessages~autociv.js
@@ -1,3 +1,13 @@
+// Chop the quantity of messages to process in a tick
+autociv_patchApplyN(XmppMessages.prototype, "handleMessages", function (target, that, args)
+{
+    const [getMessages] = args
+    Array.prototype.push.apply(that.autociv_messageQueue, getMessages() ?? [])
+    return target.apply(that, [() => that.autociv_messageQueue.splice(0, 80)])
+})
+
+XmppMessages.prototype.autociv_messageQueue = []
+
 // React to chat and messages and filter unwanted ones
 autociv_patchApplyN(XmppMessages.prototype, "handleMessages", function (target, that, args)
 {
