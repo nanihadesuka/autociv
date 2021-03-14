a7e99d18b4958d9bc7e0196c159509162d7a0a4f
nani <nani@>
2021-03-14 18:17:36 +0100

    Refactor

--- gui/common/hotkeys~autociv.js
+++ gui/common/hotkeys~autociv.js
@@ -1,14 +1,16 @@
-hotkeySort = function (a, b)
-{
-    // Quick hack to put those first.
-    if (hotkeySort.specialKeys.includes(a))
-        a = ' ' + a;
-    if (hotkeySort.specialKeys.includes(b))
-        b = ' ' + b;
-    return a.localeCompare(b, Engine.GetCurrentLocale().substr(0, 2), { "numeric": true });
-}
 
 if (!hotkeySort.specialKeys)
-    hotkeySort.specialKeys = ["Shift", "Alt", "Ctrl", "Super"];
+{
+    hotkeySort = function (a, b)
+    {
+        // Quick hack to put those first.
+        if (hotkeySort.specialKeys.includes(a))
+            a = ' ' + a;
+        if (hotkeySort.specialKeys.includes(b))
+            b = ' ' + b;
+        return a.localeCompare(b, Engine.GetCurrentLocale().substr(0, 2), { "numeric": true });
+    }
+    hotkeySort.specialKeys = ["Shift", "Alt", "Ctrl", "Super"]
+}
 
 hotkeySort.specialKeys.push("Space")
