ba23f5f30f1ce1290aea55a29d07977ca89cd0f4
nani <nani@>
2021-03-17 20:26:43 +0100

    Add player overlay for session

---
+++ gui/session/objectives/autociv_playersOverlay.js
@@ -0,0 +1,67 @@
+AutocivControls.PlayersOverlay = class
+{
+    autociv_playersOverlay = Engine.GetGUIObjectByName("autociv_playersOverlay")
+    textFont = "mono-stroke-10"
+    configKey_visible = "autociv.session.playersOverlay.visible"
+    visible = Engine.ConfigDB_GetValue("user", this.configKey_visible) == "true"
+
+    constructor()
+    {
+        if (this.visible)
+            this.update()
+
+        registerPlayerAssignmentsChangeHandler(this.onChange.bind(this))
+        registerConfigChangeHandler(this.onConfigChanges.bind(this))
+    }
+
+    onConfigChanges(changes)
+    {
+        if (changes.has(this.configKey_visible))
+        {
+            this.visible = Engine.ConfigDB_GetValue("user", this.configKey_visible) == "true"
+            this.autociv_playersOverlay.hidden = !this.visible
+        }
+        this.onChange()
+    }
+
+    onChange()
+    {
+        if (this.visible)
+            this.update()
+    }
+
+    computeSize()
+    {
+        return `100%-200 100%-10-100 100% 100%-10`
+    }
+
+    update()
+    {
+        Engine.ProfileStart("AutocivControls.PlayersOverlay:update")
+
+        const playersOffline = g_Players.
+            filter(player => player.offline).
+            map(player => [player.name, true])
+
+        const observers = Object.keys(g_PlayerAssignments).
+            filter(GUID => g_PlayerAssignments[GUID].player == -1).
+            map(GUID => [g_PlayerAssignments[GUID].name, false])
+
+        const list = [...playersOffline, ...observers]
+
+        const caption = list.map(([name, offline]) =>
+        {
+            return offline ? setStringTags(name, { "color": "250 60 30 250", }) : name
+        }).join(", ")
+
+        this.autociv_playersOverlay.hidden = !caption
+        this.autociv_playersOverlay.caption = ""
+        this.autociv_playersOverlay.size = this.computeSize(caption.length)
+        this.autociv_playersOverlay.caption = setStringTags(caption, {
+            "color": "250 250 250 250",
+            "font": this.textFont
+        })
+
+        Engine.ProfileStop()
+    }
+}
