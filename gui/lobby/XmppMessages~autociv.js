92c7d3403dc26b2f3c7ede47ab022021770d0025
nani <nani@>
2021-03-26 16:11:14 +0100

    Remove all lobby join & leave messages except the last 30 ones

--- gui/lobby/XmppMessages~autociv.js
+++ gui/lobby/XmppMessages~autociv.js
@@ -3,6 +3,21 @@ autociv_patchApplyN(XmppMessages.prototype, "handleMessages", function (target, 
 {
     const [getMessages] = args
     Array.prototype.push.apply(that.autociv_messageQueue, getMessages() ?? [])
+
+    // Remove all lobby join & leave messages except the last 30 ones
+    {
+        let count = 0
+        const max = 30
+        that.autociv_messageQueue.reverse()
+        that.autociv_messageQueue = that.autociv_messageQueue.filter(msg =>
+        {
+            if (msg.level == "join" || msg.level == "leave")
+                return count++ < max
+            return true
+        })
+        that.autociv_messageQueue.reverse()
+    }
+
     return target.apply(that, [() => that.autociv_messageQueue.splice(0, 80)])
 })
 
