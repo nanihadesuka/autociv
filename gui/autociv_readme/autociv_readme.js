0e41ba4d9dd7b72c18ce21f7b62cdb3acc091140
nani <nani@>
2021-06-22 07:48:54 +0200

    refactor

--- gui/autociv_readme/autociv_readme.js
+++ gui/autociv_readme/autociv_readme.js
@@ -3,4 +3,11 @@ function init()
 {
     Engine.GetGUIObjectByName("buttonWebpage").caption = Engine.Translate("Mod webpage")
     Engine.GetGUIObjectByName("buttonClose").caption = Engine.Translate("Close")
+    Engine.GetGUIObjectByName("title").caption = Engine.Translate("Autociv readme")
+
+    const webpageURL = "https://wildfiregames.com/forum/index.php?/topic/28753-autociv-mod-0ad-enhancer/"
+    Engine.GetGUIObjectByName("buttonWebpage").onPress = () => Engine.OpenURL(webpageURL)
+
+    const markdown = Engine.ReadFile("autociv_data/README.md")
+    Engine.GetGUIObjectByName("text").caption = autociv_SimpleMarkup(markdown)
 }
